2ceb891ce448eca625ce0edf5cbcdfff3b9205cc
Lisha Naidoo <128091406+ST10404816@users.noreply.github.com>
2024-10-25 23:06:44 +0200

    other files

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,23 @@
+﻿// Lisha Naidoo
+// ST10404816
+// Group 1
+
+// References
+// https://learn.microsoft.com/en-us/ef/core/dbcontext-configuration/
+
+using Microsoft.EntityFrameworkCore;
+using POE.Models;
+
+namespace POE.Data
+{
+    //------------------------------------------------------------------------------------------------------------------------//
+    public class AppDbContext : DbContext
+    {
+        // Constructor that accepts options for configuring the DbContext.
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        // DbSet property representing the "Claims" table in the database.
+        public DbSet<Claim> Claims { get; set; }
+    }
+}
+//------------------------------------------...ooo000 END OF FILE 000ooo...------------------------------------------------------//
